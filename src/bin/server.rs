e00b0b83498f77f3d7c7db435b71635d32ce77ae
Etienne Asselin <asselin.etienne@gmail.com>
2025-07-07 17:35:16 +0200

    Added all methods from apply blueprint to RPC server

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,7 +1,7 @@
 use std::fmt::{self, Display, Formatter};
 use actix_web::{Result,error, http::{header::ContentType, StatusCode}, post, web, App, HttpResponse, HttpServer, Responder};
-use diesel::{result::Error as dieselError};
-use evmnodetooling::dieselsqlite::{establish_connection,models::{Blueprint,Block}};
+use diesel::result::Error as dieselError;
+use evmnodetooling::dieselsqlite::{*,models::*};
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use serde_json::Error as jsonError;
 
@@ -17,12 +17,68 @@ struct Sqlquery {
 #[derive(Serialize)]
 #[serde(tag = "type")]
 enum SqlResponse{
-    BlueprintSelect{ payload:Vec<u8>, timestamp:i32},
-    BlueprintRangeSelect{idandpayloads:Vec<(i32,Vec<u8>)>},
-    BlockSelect{block:Vec<u8>},
-    BlockHashSelect{hash:Vec<u8>},
-    BlockIdSelect{id:i32},
-    NumbersofRowsAffected{number:usize},
+    BlueprintSelect{ 
+        payload:Vec<u8>, 
+        timestamp:i32
+    },
+    BlueprintRangeSelect{
+        idandpayloads:Vec<(i32,Vec<u8>)>
+    },
+    BlockSelect{
+        block:Vec<u8>
+    },
+    BlockHashSelect{
+        hash:Vec<u8>
+    },
+    BlockIdSelect{
+        id:i32
+    },
+    PendingConfirmationSelect{
+        hash:Vec<u8>
+    },
+    PendingConfirmationCount{
+        count:i64
+    },
+    TransactionReceipt{
+        block_hash:Vec<u8>,
+        block_number:i32,
+        index_:i32,
+        hash:Vec<u8>,
+        from_:Vec<u8>,
+        to_:Option<Vec<u8>>,
+        receipt_fields:Vec<u8>
+    },
+    TransactionReceipts{
+        receipts:Vec<(Vec<u8>,i32,Vec<u8>,Vec<u8>,Option<Vec<u8>>,Vec<u8>)>
+    },
+    TransactionObject{
+        block_hash:Vec<u8>,
+        block_number:i32,
+        index_:i32,
+        hash:Vec<u8>,
+        from_:Vec<u8>,
+        to_:Option<Vec<u8>>,
+        object_fields:Vec<u8>
+    },
+    TransactionObjects{
+        objects:Vec<(i32,Vec<u8>,Vec<u8>,Option<Vec<u8>>,Vec<u8>)>
+    },
+    ContextHashSelect{
+        context_hash:Vec<u8>
+    },
+    ContextHashGet{
+        id:i32,
+        context_hash:Vec<u8>
+    },
+    MetadataSmartRollupAddress{
+        address:String
+    },
+    MetadataHistoryMode{
+        history_mode:String
+    },
+    NumbersofRowsAffected{
+        number:usize
+    },
 }
 
 #[derive(Debug)]
@@ -92,7 +148,11 @@ async fn answer_query(query: web::Json<Sqlquery>) -> Result<impl Responder,Serve
             "select_blueprint"=>{
                 let id:i32=extract_parameter(&query.params[0])?;
                 let (payload,timestamp)=Blueprint::select(connection,id)?;
-                SqlResponse::BlueprintSelect{payload,timestamp}}
+                SqlResponse::BlueprintSelect{
+                    payload,
+                    timestamp
+                }
+            }
             "insert_blueprint"=>{
                 let id:i32=extract_parameter(&query.params[0])?;
                 let payload:Vec<u8>=extract_parameter(&query.params[1])?;
@@ -100,54 +160,276 @@ async fn answer_query(query: web::Json<Sqlquery>) -> Result<impl Responder,Serve
                 let blueprint=Blueprint{
                     id,payload,timestamp
                 };
-                let insertresult=blueprint.insert(connection)?;
-                SqlResponse::NumbersofRowsAffected { number:insertresult }
+                let insert_result=blueprint.insert(connection)?;
+                SqlResponse::NumbersofRowsAffected{
+                    number:insert_result 
+                }
             }
             "select_blueprint_range"=>{
                 let lowerlevel=extract_parameter(&query.params[0])?;
                 let upperlevel=extract_parameter(&query.params[1])?;
                 let idandpayloads=Blueprint::select_range(connection, lowerlevel, upperlevel)?;
-                SqlResponse::BlueprintRangeSelect {idandpayloads}
+                SqlResponse::BlueprintRangeSelect{
+                    idandpayloads
+                }
             }
             "clear_after_blueprint"=>{
                 let level:i32=extract_parameter(&query.params[0])?;
                 let clear_after_result=Blueprint::clear_after(connection, level)?;
-                SqlResponse::NumbersofRowsAffected { number:clear_after_result}
+                SqlResponse::NumbersofRowsAffected{
+                    number:clear_after_result
+                }
             }
             "clear_before_blueprint"=>{
                 let level:i32=extract_parameter(&query.params[0])?;
                 let clear_before_result=Blueprint::clear_before(connection, level)?;
-                SqlResponse::NumbersofRowsAffected { number:clear_before_result}
+                SqlResponse::NumbersofRowsAffected {
+                    number:clear_before_result
+                }
+            }
+            "insert_block"=>{
+                let level:i32=extract_parameter(&query.params[0])?;
+                let hash:Vec<u8>=extract_parameter(&query.params[1])?;
+                let block:Vec<u8>=extract_parameter(&query.params[2])?;
+                let block=Block{
+                    level,
+                    hash,
+                    block
+                };
+                let insert_result=block.insert(connection)?;
+                SqlResponse::NumbersofRowsAffected { number:insert_result }
             }
             "select_block_with_level"=>{
                 let id:i32=extract_parameter(&query.params[0])?;
                 let block=Block::select_with_level(connection, id)?;
-                SqlResponse::BlockSelect{block}
+                SqlResponse::BlockSelect{
+                    block
+                }
             }
             "select_block_with_hash"=>{
                 let hash:Vec<u8>=extract_parameter(&query.params[0])?;
                 let block=Block::select_with_hash(connection, &hash)?;
-                SqlResponse::BlockSelect{block}
+                SqlResponse::BlockSelect{
+                    block
+                }
             }
             "select_block_hash_of_number"=>{
                 let id:i32=extract_parameter(&query.params[0])?;
                 let hash=Block::select_hash_of_number(connection, id)?;
-                SqlResponse::BlockHashSelect {hash}
+                SqlResponse::BlockHashSelect {
+                    hash
+                }
             }
             "select_block_number_of_hash"=>{
                 let hash:Vec<u8>=extract_parameter(&query.params[0])?;
                 let id=Block::select_number_of_hash(connection, &hash)?;
-                SqlResponse::BlockIdSelect{id}
+                SqlResponse::BlockIdSelect{
+                    id
+                }
             }
             "clear_after_block"=>{
                 let level:i32=extract_parameter(&query.params[0])?;
                 let clear_after_result=Block::clear_after(connection, level)?;
-                SqlResponse::NumbersofRowsAffected { number:clear_after_result}
+                SqlResponse::NumbersofRowsAffected{
+                    number:clear_after_result
+                }
             }
             "clear_before_block"=>{
                 let level:i32=extract_parameter(&query.params[0])?;
                 let clear_before_result=Block::clear_before(connection, level)?;
-                SqlResponse::NumbersofRowsAffected { number:clear_before_result}
+                SqlResponse::NumbersofRowsAffected{
+                    number:clear_before_result
+                }
+            }
+            "insert_pending_confirmation"=>{
+                let level:i32=extract_parameter(&query.params[0])?;
+                let hash:Vec<u8>=extract_parameter(&query.params[1])?;
+                let pending_confirmation=PendingConfirmation{
+                    level,hash
+                };
+                let insertresult=pending_confirmation.insert(connection)?;
+                SqlResponse::NumbersofRowsAffected {
+                    number:insertresult
+                }
+            }
+            "select_pending_confirmation_with_level"=>{
+                let level:i32=extract_parameter(&query.params[0])?;
+                let hash:Vec<u8>=PendingConfirmation::select_with_level(connection, level)?;
+                SqlResponse::PendingConfirmationSelect{
+                    hash
+                }
+            }
+            "delete_pending_confirmation_with_level"=>{
+                let level:i32=extract_parameter(&query.params[0])?;
+                let delete_with_level_result=PendingConfirmation::delete_with_level(connection, level)?;
+                SqlResponse::NumbersofRowsAffected{
+                    number:delete_with_level_result
+                }
+            }
+            "clear_pending_confirmations"=>{
+                let clear_result=PendingConfirmation::clear(connection)?;
+                SqlResponse::NumbersofRowsAffected{ 
+                    number: clear_result 
+                }
+            }
+            "count_pending_confirmations"=>{
+                let count=PendingConfirmation::count(connection)?;
+                SqlResponse::PendingConfirmationCount{ 
+                    count 
+                }
+            }
+            "insert_transaction"=>{
+                let block_hash:Vec<u8>=extract_parameter(&query.params[0])?;
+                let block_number:i32=extract_parameter(&query.params[1])?;
+                let index_:i32=extract_parameter(&query.params[2])?;
+                let hash:Vec<u8>=extract_parameter(&query.params[3])?;
+                let from_:Vec<u8>=extract_parameter(&query.params[4])?;
+                let to_:Option<Vec<u8>>=extract_parameter(&query.params[5])?;
+                let receipt_fields:Vec<u8>=extract_parameter(&query.params[6])?;
+                let object_fields:Vec<u8>=extract_parameter(&query.params[7])?;
+                let transaction=Transaction{ 
+                        block_hash, 
+                        block_number, 
+                        index_, 
+                        hash, 
+                        from_, 
+                        to_, 
+                        receipt_fields, 
+                        object_fields 
+                };
+                let insert_result=transaction.insert(connection)?;
+                SqlResponse::NumbersofRowsAffected { number: insert_result }    
+            }
+            "select_transaction_receipt"=>{
+                let hash:Vec<u8>=extract_parameter(&query.params[0])?;
+                let (block_hash,block_number,index_,hash,
+                    from_,to_,receipt_fields)=
+                        Transaction::select_receipt(connection, &hash)?;
+                SqlResponse::TransactionReceipt { 
+                    block_hash, 
+                    block_number, 
+                    index_, 
+                    hash, 
+                    from_,
+                    to_,
+                    receipt_fields
+                }
+            }
+            "select_transaction_receipts_from_block_number"=>{
+                let block_number:i32=extract_parameter(&query.params[0])?;
+                let receipts=Transaction::select_receipts_from_block_number(connection, block_number)?;
+                SqlResponse::TransactionReceipts{
+                    receipts
+                }
+            }
+            "select_transaction_object"=>{
+                let hash:Vec<u8>=extract_parameter(&query.params[0])?;
+                let (block_hash,block_number,index_,hash,
+                    from_,to_,object_fields)=
+                        Transaction::select_object(connection, &hash)?;
+                SqlResponse::TransactionObject { 
+                    block_hash, 
+                    block_number, 
+                    index_, 
+                    hash, 
+                    from_,
+                    to_,
+                    object_fields
+                }
+            }
+            "select_transaction_objects_from_block_number"=>{
+                let block_number:i32=extract_parameter(&query.params[0])?;
+                let objects=Transaction::select_objects_from_block_number(connection, block_number)?;
+                SqlResponse::TransactionObjects{
+                    objects
+                }
+            }
+            "clear_transactions_after_block_number"=>{
+                let level:i32=extract_parameter(&query.params[0])?;
+                let clear_after_result=Transaction::clear_after(connection, level)?;
+                SqlResponse::NumbersofRowsAffected{
+                    number:clear_after_result
+                }
+            }
+            "clear_transactions_before_block_number"=>{
+                let level:i32=extract_parameter(&query.params[0])?;
+                let clear_after_result=Transaction::clear_before(connection, level)?;
+                SqlResponse::NumbersofRowsAffected{
+                    number:clear_after_result
+                }
+            }
+            "select_context_hash"=>{
+                let id:i32=extract_parameter(&query.params[0])?;
+                let context_hash=ContextHash::select(connection, id)?;
+                SqlResponse::ContextHashSelect { 
+                    context_hash: context_hash
+                }
+            }
+            "insert_context_hash"=>{
+                let id:i32=extract_parameter(&query.params[0])?;
+                let context_hash=extract_parameter(&query.params[1])?;
+                let context_hash=ContextHash{
+                    id,
+                    context_hash
+                };
+                let inserted_result=context_hash.insert(connection)?;
+                SqlResponse::NumbersofRowsAffected { 
+                    number: inserted_result
+                }
+            }
+            "get_latest_context_hash"=>{
+                let (id,context_hash)=ContextHash::get_latest(connection)?;
+                SqlResponse::ContextHashGet { 
+                    id, 
+                    context_hash
+                }
+            }
+            "get_earliest_context_hash"=>{
+                let (id,context_hash)=ContextHash::get_earliest(connection)?;
+                SqlResponse::ContextHashGet { 
+                    id, 
+                    context_hash
+                }
+            }
+            "clear_after_context_hash"=>{
+                let level:i32=extract_parameter(&query.params[0])?;
+                let clear_result=ContextHash::clear_after(connection, level)?;
+                SqlResponse::NumbersofRowsAffected { 
+                    number: clear_result 
+                }
+            }
+            "clear_before_context_hash"=>{
+                let level:i32=extract_parameter(&query.params[0])?;
+                let clear_result=ContextHash::clear_before(connection, level)?;
+                SqlResponse::NumbersofRowsAffected { 
+                    number: clear_result 
+                }
+            }
+            "insert_smart_rollup_address"=>{
+                let address:String=extract_parameter(&query.params[0])?;
+                let insert_result=Metadata::insert_smart_rollup_address(connection, &address)?;
+                SqlResponse::NumbersofRowsAffected { 
+                    number: insert_result 
+                } 
+            }
+            "get_smart_rollup_address"=>{
+                let address=Metadata::get_smart_rollup_address(connection)?;
+                SqlResponse::MetadataSmartRollupAddress { 
+                    address
+                }
+            }
+            "insert_history_mode"=>{
+                let history_mode:String=extract_parameter(&query.params[0])?;
+                let insert_result=Metadata::insert_history_mode(connection, &history_mode)?;
+                SqlResponse::NumbersofRowsAffected { 
+                    number: insert_result 
+                } 
+            }
+            "get_history_mode"=>{
+                let history_mode=Metadata::get_history_mode(connection)?;
+                SqlResponse::MetadataHistoryMode { 
+                    history_mode
+                }
             }
             _=>{
                 return Err(ServerError::UnknownMethod{method_name:method_requested.to_string()});
