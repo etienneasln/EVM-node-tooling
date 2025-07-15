e08fe1f95b6aa7bd96d529fbe2f7a2798cfb8ae2
Etienne Asselin <asselin.etienne@gmail.com>
2025-07-15 15:59:31 +0200

    L1L2*, Irmin chunks, block storage mode and migration RPCs

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,5 +1,6 @@
 use actix_web::{
-    App, HttpResponse, HttpServer, Responder, Result, error,
+    App, HttpResponse, HttpServer, Responder, Result,
+    error::ResponseError,
     http::{StatusCode, header::ContentType},
     post, web,
 };
@@ -18,23 +19,23 @@ struct Sqlquery {
 #[derive(Serialize)]
 #[serde(tag = "type")]
 enum SqlResponse {
-    BlueprintSelect {
+    Blueprint {
         payload: Vec<u8>,
         timestamp: i32,
     },
-    BlueprintRangeSelect {
+    BlueprintRange {
         idandpayloads: Vec<(i32, Vec<u8>)>,
     },
-    BlockSelect {
+    Block {
         block: Vec<u8>,
     },
-    BlockHashSelect {
+    BlockHash {
         hash: Vec<u8>,
     },
-    BlockIdSelect {
+    BlockId {
         id: i32,
     },
-    PendingConfirmationSelect {
+    PendingConfirmation {
         hash: Vec<u8>,
     },
     PendingConfirmationCount {
@@ -64,7 +65,7 @@ enum SqlResponse {
     TransactionObjects {
         objects: Vec<(i32, Vec<u8>, Vec<u8>, Option<Vec<u8>>, Vec<u8>)>,
     },
-    ContextHashSelect {
+    ContextHash {
         context_hash: Vec<u8>,
     },
     ContextHashGet {
@@ -103,6 +104,35 @@ enum SqlResponse {
     DelayedTransactionSelect {
         payload: Vec<u8>,
     },
+    L1L2LevelRelationshipGet {
+        latest_l2_level: i32,
+        l1_level: i32,
+    },
+    L1L2FinalizedLevelGet {
+        start_l2_level: i32,
+        end_l2_level: i32,
+    },
+    L1L2FinalizedLevel {
+        level: i32,
+    },
+    L1L2FinalizedLevelLast {
+        l1_level: i32,
+        start_l2_level: i32,
+        end_l2_level: i32,
+    },
+    L1L2FinalizedLevelList {
+        levels: Vec<(i32, i32, i32)>,
+    },
+    IrminChunk {
+        level: i32,
+        timestamp: i32,
+    },
+    BlockStorageMode{
+        legacy:i32
+    },
+    CurrentMigrationId{
+        id:i32
+    }
 }
 
 #[derive(Debug)]
@@ -113,7 +143,7 @@ enum ServerError {
     BadParameterFormat { error: jsonError },
 }
 
-impl error::ResponseError for ServerError {
+impl ResponseError for ServerError {
     fn error_response(&self) -> HttpResponse {
         HttpResponse::build(self.status_code())
             .insert_header(ContentType::html())
@@ -179,38 +209,38 @@ async fn answer_query(query: web::Json<Sqlquery>) -> Result<impl Responder, Serv
         "select_blueprint" => {
             let id = extract_parameter(&query.params[0])?;
             let (payload, timestamp) = Blueprint::select(connection, id)?;
-            SqlResponse::BlueprintSelect { payload, timestamp }
+            SqlResponse::Blueprint { payload, timestamp }
         }
         "select_blueprint_range" => {
             let lowerlevel = extract_parameter(&query.params[0])?;
             let upperlevel = extract_parameter(&query.params[1])?;
             let idandpayloads = Blueprint::select_range(connection, lowerlevel, upperlevel)?;
-            SqlResponse::BlueprintRangeSelect { idandpayloads }
+            SqlResponse::BlueprintRange { idandpayloads }
         }
         "select_block_with_level" => {
             let id = extract_parameter(&query.params[0])?;
             let block = Block::select_with_level(connection, id)?;
-            SqlResponse::BlockSelect { block }
+            SqlResponse::Block { block }
         }
         "select_block_with_hash" => {
             let hash = extract_parameter(&query.params[0])?;
             let block = Block::select_with_hash(connection, &hash)?;
-            SqlResponse::BlockSelect { block }
+            SqlResponse::Block { block }
         }
         "select_block_hash_of_number" => {
             let id = extract_parameter(&query.params[0])?;
             let hash = Block::select_hash_of_number(connection, id)?;
-            SqlResponse::BlockHashSelect { hash }
+            SqlResponse::BlockHash { hash }
         }
         "select_block_number_of_hash" => {
             let hash = extract_parameter(&query.params[0])?;
             let id = Block::select_number_of_hash(connection, &hash)?;
-            SqlResponse::BlockIdSelect { id }
+            SqlResponse::BlockId { id }
         }
         "select_pending_confirmation_with_level" => {
             let level = extract_parameter(&query.params[0])?;
             let hash: Vec<u8> = PendingConfirmation::select_with_level(connection, level)?;
-            SqlResponse::PendingConfirmationSelect { hash }
+            SqlResponse::PendingConfirmation { hash }
         }
         "count_pending_confirmations" => {
             let count = PendingConfirmation::count(connection)?;
@@ -258,7 +288,7 @@ async fn answer_query(query: web::Json<Sqlquery>) -> Result<impl Responder, Serv
         "select_context_hash" => {
             let id = extract_parameter(&query.params[0])?;
             let context_hash = ContextHash::select(connection, id)?;
-            SqlResponse::ContextHashSelect {
+            SqlResponse::ContextHash {
                 context_hash: context_hash,
             }
         }
@@ -352,6 +382,69 @@ async fn answer_query(query: web::Json<Sqlquery>) -> Result<impl Responder, Serv
             let hash = extract_parameter(&query.params[0])?;
             let payload = DelayedTransaction::select_at_hash(connection, &hash)?;
             SqlResponse::DelayedTransactionSelect { payload }
+        }
+        "get_l1_l2_level_relationship" => {
+            let (latest_l2_level, l1_level) = L1L2LevelRelationship::get(connection)?;
+            SqlResponse::L1L2LevelRelationshipGet {
+                latest_l2_level,
+                l1_level,
+            }
+        }
+        "get_l1_l2_finalized_level" => {
+            let l1_level = extract_parameter(&query.params[0])?;
+            let (start_l2_level, end_l2_level) = L1L2FinalizedLevel::get(connection, l1_level)?;
+            SqlResponse::L1L2FinalizedLevelGet {
+                start_l2_level,
+                end_l2_level,
+            }
+        }
+        "last_finalized_l2_level" => {
+            let last_l2_level = L1L2FinalizedLevel::last_l2_level(connection)?;
+            SqlResponse::L1L2FinalizedLevel {
+                level: last_l2_level,
+            }
+        }
+        "last_l1_l2_finalized_level" => {
+            let (l1_level, start_l2_level, end_l2_level) = L1L2FinalizedLevel::last(connection)?;
+            SqlResponse::L1L2FinalizedLevelLast {
+                l1_level,
+                start_l2_level,
+                end_l2_level,
+            }
+        }
+        "find_finalized_l1_level" => {
+            let l2_level = extract_parameter(&query.params[0])?;
+            let l1_level = L1L2FinalizedLevel::find_l1_level(connection, l2_level)?;
+            SqlResponse::L1L2FinalizedLevel { level: l1_level }
+        }
+        "list_l1_l1_finalized_levels_by_l2_levels" => {
+            let start_l2 = extract_parameter(&query.params[0])?;
+            let end_l2 = extract_parameter(&query.params[1])?;
+            let levels = L1L2FinalizedLevel::list_by_l2_levels(connection, start_l2, end_l2)?;
+            SqlResponse::L1L2FinalizedLevelList { levels }
+        }
+        "list_l1_l1_finalized_levels_by_l1_levels" => {
+            let start_l1 = extract_parameter(&query.params[0])?;
+            let end_l1 = extract_parameter(&query.params[1])?;
+            let levels = L1L2FinalizedLevel::list_by_l1_levels(connection, start_l1, end_l1)?;
+            SqlResponse::L1L2FinalizedLevelList { levels }
+        }
+        "nth_irmin_chunk" => {
+            let offset = extract_parameter(&query.params[0])?;
+            let (level, timestamp) = IrminChunk::nth(connection, offset)?;
+            SqlResponse::IrminChunk { level, timestamp }
+        }
+        "latest_irmin_chunk" => {
+            let (level, timestamp) = IrminChunk::latest(connection)?;
+            SqlResponse::IrminChunk { level, timestamp }
+        }
+        "block_storage_mode"=>{
+            let legacy=BlockStorageMode::legacy(connection)?;
+            SqlResponse::BlockStorageMode { legacy }
+        }
+        "current_migration"=>{
+            let id=Migration::current_migration(connection)?;
+            SqlResponse::CurrentMigrationId { id }
         }
         _ => {
             return Err(ServerError::UnknownMethod {
