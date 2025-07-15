cd4c86333ce58379d3428504b2d05beeeb3d38b3
Etienne Asselin <asselin.etienne@gmail.com>
2025-07-15 15:18:24 +0200

    Removal of writing RPCs, Implementation of upgrades and delayed transactions RPCs

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -77,8 +77,31 @@ enum SqlResponse {
     MetadataHistoryMode {
         history_mode: String,
     },
-    NumbersofRowsAffected {
-        number: usize,
+    ActivationLevels {
+        activation_levels: Vec<i32>,
+    },
+    KernelUpgradeLatestUnapplied {
+        injected_before: i32,
+        root_hash: Vec<u8>,
+        activation_timestamp: i32,
+    },
+    KernelUpgradeInjected {
+        root_hash: Vec<u8>,
+        activation_timestamp: i32,
+    },
+    SequencerUpgradeLatestUnapplied {
+        injected_before: i32,
+        sequencer: Vec<u8>,
+        pool_address: Vec<u8>,
+        activation_timestamp: i32,
+    },
+    SequencerUpgradeInjected {
+        sequencer: Vec<u8>,
+        pool_address: Vec<u8>,
+        activation_timestamp: i32,
+    },
+    DelayedTransactionSelect {
+        payload: Vec<u8>,
     },
 }
 
@@ -154,23 +177,9 @@ async fn answer_query(query: web::Json<Sqlquery>) -> Result<impl Responder, Serv
     let method_requested = query.name.as_str();
     let response = match method_requested {
         "select_blueprint" => {
-            let id: i32 = extract_parameter(&query.params[0])?;
+            let id = extract_parameter(&query.params[0])?;
             let (payload, timestamp) = Blueprint::select(connection, id)?;
             SqlResponse::BlueprintSelect { payload, timestamp }
-        }
-        "insert_blueprint" => {
-            let id: i32 = extract_parameter(&query.params[0])?;
-            let payload: Vec<u8> = extract_parameter(&query.params[1])?;
-            let timestamp: i32 = extract_parameter(&query.params[2])?;
-            let blueprint = Blueprint {
-                id,
-                payload,
-                timestamp,
-            };
-            let insert_result = blueprint.insert(connection)?;
-            SqlResponse::NumbersofRowsAffected {
-                number: insert_result,
-            }
         }
         "select_blueprint_range" => {
             let lowerlevel = extract_parameter(&query.params[0])?;
@@ -178,122 +187,37 @@ async fn answer_query(query: web::Json<Sqlquery>) -> Result<impl Responder, Serv
             let idandpayloads = Blueprint::select_range(connection, lowerlevel, upperlevel)?;
             SqlResponse::BlueprintRangeSelect { idandpayloads }
         }
-        "clear_after_blueprint" => {
-            let level: i32 = extract_parameter(&query.params[0])?;
-            let clear_after_result = Blueprint::clear_after(connection, level)?;
-            SqlResponse::NumbersofRowsAffected {
-                number: clear_after_result,
-            }
-        }
-        "clear_before_blueprint" => {
-            let level: i32 = extract_parameter(&query.params[0])?;
-            let clear_before_result = Blueprint::clear_before(connection, level)?;
-            SqlResponse::NumbersofRowsAffected {
-                number: clear_before_result,
-            }
-        }
-        "insert_block" => {
-            let level: i32 = extract_parameter(&query.params[0])?;
-            let hash: Vec<u8> = extract_parameter(&query.params[1])?;
-            let block: Vec<u8> = extract_parameter(&query.params[2])?;
-            let block = Block { level, hash, block };
-            let insert_result = block.insert(connection)?;
-            SqlResponse::NumbersofRowsAffected {
-                number: insert_result,
-            }
-        }
         "select_block_with_level" => {
-            let id: i32 = extract_parameter(&query.params[0])?;
+            let id = extract_parameter(&query.params[0])?;
             let block = Block::select_with_level(connection, id)?;
             SqlResponse::BlockSelect { block }
         }
         "select_block_with_hash" => {
-            let hash: Vec<u8> = extract_parameter(&query.params[0])?;
+            let hash = extract_parameter(&query.params[0])?;
             let block = Block::select_with_hash(connection, &hash)?;
             SqlResponse::BlockSelect { block }
         }
         "select_block_hash_of_number" => {
-            let id: i32 = extract_parameter(&query.params[0])?;
+            let id = extract_parameter(&query.params[0])?;
             let hash = Block::select_hash_of_number(connection, id)?;
             SqlResponse::BlockHashSelect { hash }
         }
         "select_block_number_of_hash" => {
-            let hash: Vec<u8> = extract_parameter(&query.params[0])?;
+            let hash = extract_parameter(&query.params[0])?;
             let id = Block::select_number_of_hash(connection, &hash)?;
             SqlResponse::BlockIdSelect { id }
         }
-        "clear_after_block" => {
-            let level: i32 = extract_parameter(&query.params[0])?;
-            let clear_after_result = Block::clear_after(connection, level)?;
-            SqlResponse::NumbersofRowsAffected {
-                number: clear_after_result,
-            }
-        }
-        "clear_before_block" => {
-            let level: i32 = extract_parameter(&query.params[0])?;
-            let clear_before_result = Block::clear_before(connection, level)?;
-            SqlResponse::NumbersofRowsAffected {
-                number: clear_before_result,
-            }
-        }
-        "insert_pending_confirmation" => {
-            let level: i32 = extract_parameter(&query.params[0])?;
-            let hash: Vec<u8> = extract_parameter(&query.params[1])?;
-            let pending_confirmation = PendingConfirmation { level, hash };
-            let insertresult = pending_confirmation.insert(connection)?;
-            SqlResponse::NumbersofRowsAffected {
-                number: insertresult,
-            }
-        }
         "select_pending_confirmation_with_level" => {
-            let level: i32 = extract_parameter(&query.params[0])?;
+            let level = extract_parameter(&query.params[0])?;
             let hash: Vec<u8> = PendingConfirmation::select_with_level(connection, level)?;
             SqlResponse::PendingConfirmationSelect { hash }
-        }
-        "delete_pending_confirmation_with_level" => {
-            let level: i32 = extract_parameter(&query.params[0])?;
-            let delete_with_level_result =
-                PendingConfirmation::delete_with_level(connection, level)?;
-            SqlResponse::NumbersofRowsAffected {
-                number: delete_with_level_result,
-            }
-        }
-        "clear_pending_confirmations" => {
-            let clear_result = PendingConfirmation::clear(connection)?;
-            SqlResponse::NumbersofRowsAffected {
-                number: clear_result,
-            }
         }
         "count_pending_confirmations" => {
             let count = PendingConfirmation::count(connection)?;
             SqlResponse::PendingConfirmationCount { count }
         }
-        "insert_transaction" => {
-            let block_hash: Vec<u8> = extract_parameter(&query.params[0])?;
-            let block_number: i32 = extract_parameter(&query.params[1])?;
-            let index_: i32 = extract_parameter(&query.params[2])?;
-            let hash: Vec<u8> = extract_parameter(&query.params[3])?;
-            let from_: Vec<u8> = extract_parameter(&query.params[4])?;
-            let to_: Option<Vec<u8>> = extract_parameter(&query.params[5])?;
-            let receipt_fields: Vec<u8> = extract_parameter(&query.params[6])?;
-            let object_fields: Vec<u8> = extract_parameter(&query.params[7])?;
-            let transaction = Transaction {
-                block_hash,
-                block_number,
-                index_,
-                hash,
-                from_,
-                to_,
-                receipt_fields,
-                object_fields,
-            };
-            let insert_result = transaction.insert(connection)?;
-            SqlResponse::NumbersofRowsAffected {
-                number: insert_result,
-            }
-        }
         "select_transaction_receipt" => {
-            let hash: Vec<u8> = extract_parameter(&query.params[0])?;
+            let hash = extract_parameter(&query.params[0])?;
             let (block_hash, block_number, index_, hash, from_, to_, receipt_fields) =
                 Transaction::select_receipt(connection, &hash)?;
             SqlResponse::TransactionReceipt {
@@ -307,13 +231,13 @@ async fn answer_query(query: web::Json<Sqlquery>) -> Result<impl Responder, Serv
             }
         }
         "select_transaction_receipts_from_block_number" => {
-            let block_number: i32 = extract_parameter(&query.params[0])?;
+            let block_number = extract_parameter(&query.params[0])?;
             let receipts =
                 Transaction::select_receipts_from_block_number(connection, block_number)?;
             SqlResponse::TransactionReceipts { receipts }
         }
         "select_transaction_object" => {
-            let hash: Vec<u8> = extract_parameter(&query.params[0])?;
+            let hash = extract_parameter(&query.params[0])?;
             let (block_hash, block_number, index_, hash, from_, to_, object_fields) =
                 Transaction::select_object(connection, &hash)?;
             SqlResponse::TransactionObject {
@@ -327,38 +251,15 @@ async fn answer_query(query: web::Json<Sqlquery>) -> Result<impl Responder, Serv
             }
         }
         "select_transaction_objects_from_block_number" => {
-            let block_number: i32 = extract_parameter(&query.params[0])?;
+            let block_number = extract_parameter(&query.params[0])?;
             let objects = Transaction::select_objects_from_block_number(connection, block_number)?;
             SqlResponse::TransactionObjects { objects }
         }
-        "clear_transactions_after_block_number" => {
-            let level: i32 = extract_parameter(&query.params[0])?;
-            let clear_after_result = Transaction::clear_after(connection, level)?;
-            SqlResponse::NumbersofRowsAffected {
-                number: clear_after_result,
-            }
-        }
-        "clear_transactions_before_block_number" => {
-            let level: i32 = extract_parameter(&query.params[0])?;
-            let clear_after_result = Transaction::clear_before(connection, level)?;
-            SqlResponse::NumbersofRowsAffected {
-                number: clear_after_result,
-            }
-        }
         "select_context_hash" => {
-            let id: i32 = extract_parameter(&query.params[0])?;
+            let id = extract_parameter(&query.params[0])?;
             let context_hash = ContextHash::select(connection, id)?;
             SqlResponse::ContextHashSelect {
                 context_hash: context_hash,
-            }
-        }
-        "insert_context_hash" => {
-            let id: i32 = extract_parameter(&query.params[0])?;
-            let context_hash = extract_parameter(&query.params[1])?;
-            let context_hash = ContextHash { id, context_hash };
-            let inserted_result = context_hash.insert(connection)?;
-            SqlResponse::NumbersofRowsAffected {
-                number: inserted_result,
             }
         }
         "get_latest_context_hash" => {
@@ -369,41 +270,88 @@ async fn answer_query(query: web::Json<Sqlquery>) -> Result<impl Responder, Serv
             let (id, context_hash) = ContextHash::get_earliest(connection)?;
             SqlResponse::ContextHashGet { id, context_hash }
         }
-        "clear_after_context_hash" => {
-            let level: i32 = extract_parameter(&query.params[0])?;
-            let clear_result = ContextHash::clear_after(connection, level)?;
-            SqlResponse::NumbersofRowsAffected {
-                number: clear_result,
-            }
-        }
-        "clear_before_context_hash" => {
-            let level: i32 = extract_parameter(&query.params[0])?;
-            let clear_result = ContextHash::clear_before(connection, level)?;
-            SqlResponse::NumbersofRowsAffected {
-                number: clear_result,
-            }
-        }
-        "insert_smart_rollup_address" => {
-            let address: String = extract_parameter(&query.params[0])?;
-            let insert_result = Metadata::insert_smart_rollup_address(connection, &address)?;
-            SqlResponse::NumbersofRowsAffected {
-                number: insert_result,
-            }
-        }
         "get_smart_rollup_address" => {
             let address = Metadata::get_smart_rollup_address(connection)?;
             SqlResponse::MetadataSmartRollupAddress { address }
         }
-        "insert_history_mode" => {
-            let history_mode: String = extract_parameter(&query.params[0])?;
-            let insert_result = Metadata::insert_history_mode(connection, &history_mode)?;
-            SqlResponse::NumbersofRowsAffected {
-                number: insert_result,
-            }
-        }
         "get_history_mode" => {
             let history_mode = Metadata::get_history_mode(connection)?;
             SqlResponse::MetadataHistoryMode { history_mode }
+        }
+        "kernel_upgrade_activation_levels" => {
+            let activation_levels = KernelUpgrade::activation_levels(connection)?;
+            SqlResponse::ActivationLevels { activation_levels }
+        }
+        "get_latest_unapplied_kernel_upgrade" => {
+            let (injected_before, root_hash, activation_timestamp) =
+                KernelUpgrade::get_latest_unapplied(connection)?;
+            SqlResponse::KernelUpgradeLatestUnapplied {
+                injected_before,
+                root_hash,
+                activation_timestamp,
+            }
+        }
+        "find_kernel_upgrade_injected_before" => {
+            let injected_before = extract_parameter(&query.params[0])?;
+            let (root_hash, activation_timestamp) =
+                KernelUpgrade::find_injected_before(connection, injected_before)?;
+            SqlResponse::KernelUpgradeInjected {
+                root_hash,
+                activation_timestamp,
+            }
+        }
+        "find_latest_kernel_upgrade_injected_after" => {
+            let injected_after = extract_parameter(&query.params[0])?;
+            let (root_hash, activation_timestamp) =
+                KernelUpgrade::find_latest_injected_after(connection, injected_after)?;
+            SqlResponse::KernelUpgradeInjected {
+                root_hash,
+                activation_timestamp,
+            }
+        }
+        "sequencer_upgrade_activation_levels" => {
+            let activation_levels = SequencerUpgrade::activation_levels(connection)?;
+            SqlResponse::ActivationLevels { activation_levels }
+        }
+        "get_latest_unapplied_sequencer_upgrade" => {
+            let (injected_before, sequencer, pool_address, activation_timestamp) =
+                SequencerUpgrade::get_latest_unapplied(connection)?;
+            SqlResponse::SequencerUpgradeLatestUnapplied {
+                injected_before,
+                sequencer,
+                pool_address,
+                activation_timestamp,
+            }
+        }
+        "find_sequencer_upgrade_injected_before" => {
+            let injected_before = extract_parameter(&query.params[0])?;
+            let (sequencer, pool_address, activation_timestamp) =
+                SequencerUpgrade::find_injected_before(connection, injected_before)?;
+            SqlResponse::SequencerUpgradeInjected {
+                sequencer,
+                pool_address,
+                activation_timestamp,
+            }
+        }
+        "find_latest_sequencer_upgrade_injected_after" => {
+            let injected_after = extract_parameter(&query.params[0])?;
+            let (sequencer, pool_address, activation_timestamp) =
+                SequencerUpgrade::find_latest_injected_after(connection, injected_after)?;
+            SqlResponse::SequencerUpgradeInjected {
+                sequencer,
+                pool_address,
+                activation_timestamp,
+            }
+        }
+        "select_delayed_transaction_at_level" => {
+            let injected_before = extract_parameter(&query.params[0])?;
+            let payload = DelayedTransaction::select_at_level(connection, injected_before)?;
+            SqlResponse::DelayedTransactionSelect { payload }
+        }
+        "select_delayed_transaction_at_hash" => {
+            let hash = extract_parameter(&query.params[0])?;
+            let payload = DelayedTransaction::select_at_hash(connection, &hash)?;
+            SqlResponse::DelayedTransactionSelect { payload }
         }
         _ => {
             return Err(ServerError::UnknownMethod {
