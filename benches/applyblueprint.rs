598fa47d0eebe9ece3c521f33ccc5a7b324d2517
Etienne Asselin <asselin.etienne@gmail.com>
2025-07-30 16:18:07 +0200

    Moved block and transaction hash generation inside benchmarked function to reflect OCaml benchmark. Fixed transaction inserting/clearing bug (didn't clear before)

--- benches/applyblueprint.rs
+++ benches/applyblueprint.rs
@@ -48,7 +48,7 @@ fn criterion_insert_block(c: &mut Criterion) {
     let mut insert_id = clear_id + 1;
 
     let block = Block::select_with_level(connection, select_id).unwrap();
-    let bytes = [0u8; 32];
+    let bytes = &mut [0u8; 32];
 
     connection
         .transaction::<_, Error, _>(|conn| {
@@ -64,7 +64,7 @@ fn criterion_insert_block(c: &mut Criterion) {
 fn run_insert_block(
     connection: &mut SqliteConnection,
     level: &mut i32,
-    bytes: [u8; 32],
+    bytes: &mut [u8; 32],
     block: &Vec<u8>,
 ) {
     let block = Block {
@@ -83,14 +83,16 @@ fn criterion_insert_transactions(c: &mut Criterion) {
     let select_id = block_number;
     let clear_id = Block::top_level(connection).unwrap();
     let mut insert_id = clear_id + 1;
-    let bytes = [0u8; 32];
+
+    let transactions = select_transactions(connection, select_id);
+    let bytes = &mut [0u8; 32];
 
     connection
         .transaction::<_, Error, _>(|conn| {
             c.bench_function("step Insert transactions", |b| {
-                b.iter(|| run_insert_transaction(conn, select_id, &mut insert_id, bytes))
+                b.iter(|| run_insert_transaction(conn, &transactions, &mut insert_id, bytes))
             });
-            let _ = Block::clear_after(conn, clear_id);
+            let _ = Transaction::clear_after(conn, clear_id);
             Ok(())
         })
         .unwrap();
@@ -98,15 +100,16 @@ fn criterion_insert_transactions(c: &mut Criterion) {
 
 fn run_insert_transaction(
     connection: &mut SqliteConnection,
-    select_id: i32,
+    transactions: &Vec<(Vec<u8>, i32, Vec<u8>, Option<Vec<u8>>, Vec<u8>, Vec<u8>)>,
     insert_id: &mut i32,
-    bytes: [u8; 32],
+    bytes: &mut [u8; 32],
 ) {
-    let transactions = transactions(connection, select_id, *insert_id, bytes);
+    let transactions = generate_transactions_with_hash(transactions, *insert_id, bytes);
 
     for tx in transactions {
         let _ = tx.insert(connection);
     }
+
     *insert_id += 1;
 }
 
@@ -164,69 +167,72 @@ fn run_select_history_mode(connection: &mut SqliteConnection) {
 fn criterion_apply_blueprint(c: &mut Criterion) {
     let block_number = load_block_number();
 
-    let mut connection = establish_connection().unwrap();
+    let connection = &mut establish_connection().unwrap();
     let select_id = block_number;
-    let clear_id = Blueprint::top_level(&mut connection).unwrap();
-    let insert_id = clear_id + 1;
+    let base_insert_id = Blueprint::top_level(connection).unwrap();
+    let (payload, timestamp) = Blueprint::select(connection, select_id).unwrap();
+    let block = Block::select_with_level(connection, select_id).unwrap();
+    let bytes = &mut [0u8; 32];
 
-    let setup = || {
-        let mut connection = establish_connection().unwrap();
-
-        let (payload, timestamp) = Blueprint::select(&mut connection, select_id).unwrap();
-        let blueprint = Blueprint {
-            id: insert_id,
-            payload,
-            timestamp,
-        };
-        let block = Block::select_with_level(&mut connection, select_id).unwrap();
-        let bytes = [0u8; 32];
-
-        let block = Block {
-            level: insert_id,
-            hash: rand_32_bytes_vec(bytes),
-            block,
-        };
-        let transactions = transactions(&mut connection, select_id, insert_id, bytes);
-        let context_hash = ContextHash {
-            id: insert_id,
-            context_hash: ContextHash::select(&mut connection, select_id).unwrap(),
-        };
-        clear_after_apply_blueprint(&mut connection, clear_id);
-        (connection, blueprint, block, transactions, context_hash)
-    };
-
-    let routine = |(mut connection, blueprint, block, transactions, context_hash)| {
-        run_apply_blueprint(
-            &mut connection,
-            blueprint,
-            block,
-            transactions,
-            context_hash,
-        )
-    };
+    let mut insert_id = base_insert_id + 1;
+    let transactions = select_transactions(connection, select_id);
+    let context_hash = ContextHash::select(connection, select_id).unwrap();
 
     c.bench_function("Apply blueprint", |b| {
-        b.iter_batched(setup, routine, criterion::BatchSize::PerIteration)
+        b.iter(|| {
+            run_apply_blueprint(
+                connection,
+                &mut insert_id,
+                &payload,
+                timestamp,
+                &block,
+                &transactions,
+                &context_hash,
+                bytes,
+            )
+        })
     });
+
+    let _ = clear_after_apply_blueprint(connection, base_insert_id);
 }
 
 fn run_apply_blueprint(
     connection: &mut SqliteConnection,
-    blueprint: Blueprint,
-    block: Block,
-    transactions: Vec<Transaction>,
-    context_hash: ContextHash,
+    insert_id: &mut i32,
+    payload: &Vec<u8>,
+    timestamp: i32,
+    block: &Vec<u8>,
+    transactions: &Vec<(Vec<u8>, i32, Vec<u8>, Option<Vec<u8>>, Vec<u8>, Vec<u8>)>,
+    context_hash: &Vec<u8>,
+    bytes: &mut [u8; 32],
 ) {
     connection
         .transaction::<_, Error, _>(|conn| {
-            let _ = PendingConfirmation::select_with_level(conn, blueprint.id);
+            let _ = PendingConfirmation::select_with_level(conn, *insert_id);
+
+            let blueprint = Blueprint {
+                id: *insert_id,
+                payload: payload.clone(),
+                timestamp,
+            };
             blueprint.insert(conn)?;
+            let block = Block {
+                level: *insert_id,
+                hash: rand_32_bytes_vec(bytes),
+                block: block.clone(),
+            };
             block.insert(conn)?;
+            let transactions = generate_transactions_with_hash(&transactions, *insert_id, bytes);
             for tx in transactions {
                 tx.insert(conn)?;
             }
+            let context_hash = ContextHash {
+                id: *insert_id,
+                context_hash: context_hash.clone(),
+            };
             context_hash.insert(conn)?;
             let _history_mode = Metadata::get_history_mode(conn)?;
+            *insert_id += 1;
             Ok(())
         })
         .unwrap();
@@ -245,12 +251,10 @@ criterion_main!(benches);
 
 //Helper functions
 
-fn transactions(
+fn select_transactions(
     connection: &mut SqliteConnection,
     select_id: i32,
-    insert_id: i32,
-    bytes: [u8; 32],
-) -> Vec<Transaction> {
+) -> Vec<(Vec<u8>, i32, Vec<u8>, Option<Vec<u8>>, Vec<u8>, Vec<u8>)> {
     let transactions_receipts =
         Transaction::select_receipts_from_block_number(connection, select_id).unwrap();
     let transaction_objects =
@@ -261,16 +265,36 @@ fn transactions(
         .zip(transaction_objects.into_iter())
         .map(
             |((block_hash, index_, _, from_, to_, receipt_fields), (_, _, _, _, object_fields))| {
-                Transaction {
+                (
                     block_hash,
-                    block_number: insert_id,
                     index_,
-                    hash: rand_32_bytes_vec(bytes),
                     from_,
                     to_,
                     receipt_fields,
                     object_fields,
-                }
+                )
+            },
+        )
+        .collect::<Vec<(Vec<u8>, i32, Vec<u8>, Option<Vec<u8>>, Vec<u8>, Vec<u8>)>>()
+}
+
+fn generate_transactions_with_hash(
+    transactions: &Vec<(Vec<u8>, i32, Vec<u8>, Option<Vec<u8>>, Vec<u8>, Vec<u8>)>,
+    block_number: i32,
+    bytes: &mut [u8; 32],
+) -> Vec<Transaction> {
+    transactions
+        .into_iter()
+        .map(
+            |(block_hash, index_, from_, to_, receipt_fields, object_fields)| Transaction {
+                block_hash: block_hash.clone(),
+                block_number,
+                index_: index_.clone(),
+                hash: rand_32_bytes_vec(bytes),
+                from_: from_.clone(),
+                to_: to_.clone(),
+                receipt_fields: receipt_fields.clone(),
+                object_fields: object_fields.clone(),
             },
         )
         .collect::<Vec<Transaction>>()
@@ -283,7 +307,7 @@ fn clear_after_apply_blueprint(connection: &mut SqliteConnection, clear_id: i32)
     let _ = Transaction::clear_after(connection, clear_id);
 }
 
-fn rand_32_bytes_vec(mut bytes: [u8; 32]) -> Vec<u8> {
-    rand::fill(&mut bytes);
+fn rand_32_bytes_vec(bytes: &mut [u8; 32]) -> Vec<u8> {
+    rand::fill(bytes);
     Vec::from(bytes)
 }
